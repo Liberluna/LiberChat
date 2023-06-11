bf9d754641721dff6e13b456b2b6e2dd63f4cad6
nakasyou <syoutarou1103@outlook.jp>
2023-06-11 01:42:10 +0000

    gen

--- routes/_export.ts
+++ routes/_export.ts
@@ -3,16 +3,18 @@
 
 import * as $0 from "./_app.tsx";
 import * as $1 from "./_404.tsx";
-import * as $2 from "./todos.tsx";
-import * as $3 from "./index.tsx";
-import * as $4 from "./socket/test.tsx";
-import * as $5 from "./socket/comet.ts";
+import * as $2 from "./into.tsx";
+import * as $3 from "./todos.tsx";
+import * as $4 from "./index.tsx";
+import * as $5 from "./socket/test.tsx";
+import * as $6 from "./socket/comet.ts";
 
 export default {
   "/_app": $0,
   "/_404": $1,
-  "/todos": $2,
-  "/": $3,
-  "/socket/test": $4,
-  "/socket/comet": $5,
+  "/into": $2,
+  "/todos": $3,
+  "/": $4,
+  "/socket/test": $5,
+  "/socket/comet": $6,
 };
