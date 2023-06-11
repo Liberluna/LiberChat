b59115f7147a8e504b9b47730f15de81ea573454
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-11 13:07:44 +0900

    Update Heads.tsx

--- components/Heads.tsx
+++ components/Heads.tsx
@@ -1,7 +1,7 @@
 import { Head } from 'aleph/react'; 
 
 export default function Heads() {
-  return (
+  return <>
     <Head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,5 +9,5 @@ export default function Heads() {
       <meta name="description" content="Liberchat : 更新不要なリアルタイムチャットを最高峰のUI/UXで、Liberlunaが提供します。">
       <link rel="icon" href="/assets/icon.svg">
     </Head>
-  );
+  </>
 }
