2558b6e35089a79fe808acf6407d5aaa82ef0d94
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-11 15:30:57 +0900

    Update messages.tsx

--- components/chat/messages.tsx
+++ components/chat/messages.tsx
@@ -1,10 +1,12 @@
+import { type Message } from "~/system/chat/index.ts"
+
 export interface MessagesProps {
-  messages: any[]
+  messages: Message[]
 }
 export default (props: MessagesProps) => {
   return props.messages.map(message => {
     return <div>
-      <div>{ message.name }</div>
+      <div>{ message.user }</div>
       <div>{ message.body }</div>
     </div>
   })
