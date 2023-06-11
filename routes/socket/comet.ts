33b75e33c52127613b98d61dac275c71c5f788e9
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-11 10:27:18 +0900

    Update comet.ts

--- routes/socket/comet.ts
+++ routes/socket/comet.ts
@@ -1,12 +1,28 @@
 import { Context } from "aleph/server"
 import * as res from "~/utils/res/index.ts"
 
+const resolves = []
+
 export function GET (req: Request, ctx: Context) {
   const params = new URL(req.url).searchParams
   
+  const room = params.get("room")
+  
+  if(!resolves[room]){
+    resolves[room] = []
+  }
   if(params.has("msg")){
+    for(const resolve of resolves[room]){
+      resolve(res.json({
+        msg: params.get("msg")
+      }))
+    }
     return res.json({
       msg: params.get("msg")
     })
   }
+  
+  return new Promise((resolve) => {
+    resolves[room].push(resolve)
+  })
 }
