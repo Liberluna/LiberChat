3465b49e6233a01920094b4ec49588823a6f6406
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-11 12:11:18 +0900

    Update $roomid.tsx

--- routes/room/$roomid.tsx
+++ routes/room/$roomid.tsx
@@ -1,11 +1,12 @@
 import { type Context } from "aleph/server"
 
 export default function(req: Request, ctx: Context) {
-  console.log(ctx)
+  const url = new URL(req.url)
+  const roomId = url.pathname.split("/").at(-1)
   return <>
     <div>
       This room name is
-      { ctx.params.roomid }
+      { roomId }
     </div>
   </>
 }
