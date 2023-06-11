43d023e08e5932063c51e5640496f955901b0013
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-11 13:54:52 +0900

    必要のないものの削除
    コンフリクトfix

--- components/Heads.tsx
+++ components/Heads.tsx
@@ -2,9 +2,7 @@ import { Head } from 'aleph/react';
 
 export default function Heads() {
   return <>
-    <Head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <Head
       <title>Liberchat</title>
       <meta name="description" content="Liberchat : 更新不要なリアルタイムチャットを最高峰のUI/UXで、Liberlunaが提供します。" />
       <link rel="icon" href="/assets/icon.svg" />
