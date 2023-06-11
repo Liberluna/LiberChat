cb4a095885a9106dcd704f8525c7c5ab61dcc326
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-11 13:27:17 +0900

    Update index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,11 +4,11 @@ import { Head, Link } from "aleph/react"
 export default function () {
   return <>
     <Head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>Liberchat</title>
-      <meta name="description" content="Liberchat : 更新不要なリアルタイムチャットを最高峰のUI/UXで、Liberlunaが提供します。">
-      <link rel="icon" href="/assets/icon.svg">
+      <meta name="description" content="Liberchat : 更新不要なリアルタイムチャットを最高峰のUI/UXで、Liberlunaが提供します。" />
+      <link rel="icon" href="/assets/icon.svg" />
     </Head>
     <div className="mx-10">
       <div className="text-center text-2xl">LiberChat</div>
