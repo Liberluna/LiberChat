29dbd950d3abc1b4363244d430ca9701af0e33ce
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-11 13:11:32 +0900

    test

--- routes/room/$roomid.tsx
+++ routes/room/$roomid.tsx
@@ -1,5 +1,6 @@
 import { type Context } from "aleph/server"
 import { useData } from "aleph/react"
+import { useEffect } from "react"
 
 export function data(req: Request, ctx: Context) {
   const url = new URL(req.url)
@@ -8,9 +9,27 @@ export function data(req: Request, ctx: Context) {
     roomid: url.pathname.split("/").at(-1),
   }
 }
+
+const connect = async () => {
+  const { data } = useData()
+  alert(data.roomid)
+  
+  const res = await fetch("/socket/commet, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: JSON.stringify({
+      
+    })
+  })
+}
 export default function() {
   const { data } = useData()
   
+  useEffect(()=>{
+    connect()
+  },[])
   return <>
     <div>
       {data.roomid}
