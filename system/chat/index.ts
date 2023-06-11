ee164e47793f34b735bde53410b7bbb8265486ae
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-11 15:29:41 +0900

    fix

--- system/chat/index.ts
+++ system/chat/index.ts
@@ -2,18 +2,7 @@
  * メッセージのType定義
  */
 export interface Message {
-  text: string
+  body: string
   user: string
   room: string
-}
-class Listener {
-  constructor(room: string){
-    const kv = await Deno.openKv()
-    this.promise = new Promise((resolve) => {
-      
-    })
-  }
-  waitRes () {
-    
-  }
 }
