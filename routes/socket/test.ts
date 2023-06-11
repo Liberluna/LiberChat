65406122f32e75ec9edb9087df7ffdaea0323756
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-11 09:56:24 +0900

    Create test.ts

---
+++ routes/socket/test.ts
@@ -0,0 +1,21 @@
+export function GET () {
+  return new Response(`<!doctype HTML>
+  <html>
+    <head>
+    </head>
+    <body>
+      <h1>SSE TEST</h1>
+      <script>
+      alert("loaded")
+      const sse = new EventSource()
+      sse.addEventListener("message", (e) => {
+        alert(e.data)
+      });
+      </script>
+    </body>
+  </html>`,{
+    headers: {
+      "content-type": "text/html"
+    }
+  })
+}
