806f262c41f80f0d3749bd51b4af599bf97e6461
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-11 10:38:06 +0900

    Link to

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="sticky top-0 w-full h-20">
       <div className="flex">
         <div class="bold">
-          <Link href="/">LiberChat</Link>
+          <Link to="/">LiberChat</Link>
         </div>
       </div>
     </header>
