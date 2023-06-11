b5253a5f553ce8bd289463dde817bb87dd1a20cb
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-11 10:22:35 +0900

    Update comet.ts

--- routes/socket/comet.ts
+++ routes/socket/comet.ts
@@ -1,26 +1,12 @@
 import { Context } from "aleph/server"
 import * as res from "~/utils/res/index.ts"
-import * as chat from "~/system/chat/index.ts"
 
 export function GET (req: Request, ctx: Context) {
-  let timeoutId: number | undefined;
-  console.log("add conn")
-  const stream = new ReadableStream({
-    start(controller) {
-      timeoutId = setInterval(() => {
-        // utf8でエンコード
-        const data = new TextEncoder().encode(`data: ${new Date()}\n\n`);
-        // 送信
-        controller.enqueue(data);
-      }, 1000); // 1秒おきに送信
-    },
-    cancel() {
-      // 接続切断時のクリーンアップ
-      console.log("cancel")
-      clearInterval(timeoutId);
-    },
-  });
-  return new Response(stream, {
-    headers: { "Content-Type": "text/event-stream; charset=utf-8" },
-  });
+  const params = new URL(req.url).searchParams
+  
+  if(params.has("msg")){
+    return res.json({
+      msg: params.get("msg")
+    })
+  }
 }
