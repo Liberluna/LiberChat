8dc9425432cbc3eafdb517eb1b64447a4b4fc64e
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-11 09:35:58 +0900

    Update index.ts

--- system/chat/index.ts
+++ system/chat/index.ts
@@ -6,31 +6,8 @@ export interface Message {
   user: string
   room: string
 }
-
-export type Callback = (Message) => void
-
-const callbacks: Record<string, Callback[]>
-/**
- * サーバーがメッセージを受け取る
- */
-export function from (msg: Message) {
-  if(!callbacks[msg.room]){
-    return
-  }
-  
-  for(const callback of callbacks[msg.room]) {
-    callback(msg)
-  }
-  callbacks[msg.room] = []
-}
-/**
- * メッセージがくるまでWait
- */
-export function getMessage(room: string) {
-  return new Promise((resolve) => {
-    if(!callbacks[room]){
-      callbacks[room] = []
-    }
-    callbacks[room].push(resolve)
+export function waitRes () {
+  return new Promise(resolve => {
+    setTimeout(e=>resolve({hello:"world"},100)
   })
 }
