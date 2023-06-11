cc8f3c59e2989aec6b15c6069cbd0cd3bff019ce
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-11 10:37:30 +0900

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,11 @@ import { Link } from "aleph/react";
 export default function Header() {
   return (
     <header className="sticky top-0 w-full h-20">
-      
+      <div className="flex">
+        <div class="bold">
+          <Link href="/">LiberChat</Link>
+        </div>
+      </div>
     </header>
   );
 }
