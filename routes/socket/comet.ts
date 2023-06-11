793c7a1c9662c1dee5f239ffabb5deeffa3c871d
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-11 09:24:45 +0900

    fix

--- routes/socket/comet.ts
+++ routes/socket/comet.ts
@@ -5,7 +5,7 @@ import * as chat from "~/system/chat/index.ts"
 let count = 0
 
 export async function GET (req: Request, ctx: Context) {
-  await chat.getMessage("x")
+  //await chat.getMessage("x")
   return res.json({
     status: "ok!",
   })
