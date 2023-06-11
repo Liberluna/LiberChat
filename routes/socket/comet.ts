83696f5adde067ed32eabe0dd525270ab01073a2
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-11 09:24:59 +0900

    remove count

--- routes/socket/comet.ts
+++ routes/socket/comet.ts
@@ -2,8 +2,6 @@ import { Context } from "aleph/server"
 import * as res from "~/utils/res/index.ts"
 import * as chat from "~/system/chat/index.ts"
 
-let count = 0
-
 export async function GET (req: Request, ctx: Context) {
   //await chat.getMessage("x")
   return res.json({
