b7ef98041bdaa4ff79f867f2df5672ff5c0b3d41
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-11 11:37:59 +0900

    intoroom

--- routes/into.tsx
+++ routes/into.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 export default function() {
   const css = `
       .btn:hover {
@@ -16,6 +18,13 @@ export default function() {
         }
       }
     `
+  const [name, setName] = useState("")
+  const [roomId, setRoomId] = useState("")
+  const intoRoom = () => {
+    
+    const url = `/chat/room/${roomId}?userName`
+    alert(url)
+  }
   return <>
     <div className="mx-10">
       <div className="text-center text-2xl">
@@ -24,11 +33,15 @@ export default function() {
       <div class="mx-auto">
         <div className="my-5">
           <label>Name: </label>
-          <input placeholder="name" className="border" />
+          <input placeholder="name" className="border" value={name} onInput={(e)=>{
+            setRoomId(e.target.value)
+          }}/>
         </div>
         <div className="my-5">
           <label>Room id: </label>
-          <input placeholder="Room ID" className="border" />
+          <input placeholder="Room ID" className="border" value={roomId} onInput={(e)=>{
+            setRoomId(e.target.value.replaceAll(/[^a-zA-Z0-9]/,"").toLowerCase())
+          }} />
         </div>
         <div>
           <button className="btn p-5 text-white bg-dark rd text-xl">入室</button>
