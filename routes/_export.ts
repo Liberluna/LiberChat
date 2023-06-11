55a5917b4e81526acc383d74108e027392d323fd
nakasyou <syoutarou1103@outlook.jp>
2023-06-11 01:02:04 +0000

    gen

--- routes/_export.ts
+++ routes/_export.ts
@@ -1,16 +1,18 @@
 // Exports router modules for serverless env that doesn't support the dynamic import.
 // This module will be updated automaticlly in develoment mode, do NOT edit it manually.
 
-import * as $0 from "./_404.tsx";
-import * as $1 from "./_app.tsx";
-import * as $2 from "./index.tsx";
-import * as $3 from "./todos.tsx";
-import * as $4 from "./socket/comet.ts";
+import * as $0 from "./_app.tsx";
+import * as $1 from "./_404.tsx";
+import * as $2 from "./todos.tsx";
+import * as $3 from "./index.tsx";
+import * as $4 from "./socket/test.tsx";
+import * as $5 from "./socket/comet.ts";
 
 export default {
-  "/_404": $0,
-  "/_app": $1,
-  "/": $2,
-  "/todos": $3,
-  "/socket/comet": $4,
+  "/_app": $0,
+  "/_404": $1,
+  "/todos": $2,
+  "/": $3,
+  "/socket/test": $4,
+  "/socket/comet": $5,
 };
