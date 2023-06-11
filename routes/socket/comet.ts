468561089597748a3ae6b6b02045e69e4ce2a67a
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-11 09:08:41 +0900

    counter test

--- routes/socket/comet.ts
+++ routes/socket/comet.ts
@@ -1,11 +1,16 @@
 import { Context } from "aleph/server"
 import * as res from "~/utils/res/index.ts"
 
+let count = 0
+
 export function GET (req: Request, ctx: Context) {
+  count++
+  
   return new Promise((resolve) => {
     setTimeout(()=>{
       resolve(res.json({
-        result: 0
+        result: 0,
+        count,
       }))
     }, 2000)
   })
