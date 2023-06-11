ada1cfc5d6305f789976c5097f8909d486d2d721
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-11 13:38:30 +0900

    roomId

--- routes/room/$roomid.tsx
+++ routes/room/$roomid.tsx
@@ -6,34 +6,39 @@ export function data(req: Request, ctx: Context) {
   const url = new URL(req.url)
   
   return {
-    roomid: url.pathname.split("/").at(-1),
+    roomId: url.pathname.split("/").at(-1),
   }
 }
 
-const connect = async () => {
-  const { data } = useData()
-  alert(data.roomid)
-  
+const connect = async (args) => {
+  const { roomId } = args
+  alert(roomId)
   const res = await fetch("/socket/commet", {
     method: "POST",
     headers: {
       'Content-Type': 'application/json',
     },
     data: JSON.stringify({
-      
+      room: roomId,
     })
   })
+  if(res.status === 200){
+    // OK
+    const json = await res.json()
+  }
+  roomId(args)
 }
 export default function() {
   const { data } = useData()
   
   useEffect(()=>{
-    alert(0)
-    connect()
+    connect({
+      roomId: data.roomId,
+    })
   },[])
   return <>
     <div>
-      {data.roomid}
+      {data.roomId}
     </div>
   </>
 }
