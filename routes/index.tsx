950bab1509e9d2dd300c543bd81a45026075cc3e
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-11 13:53:01 +0900

    必要のないものを削除

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,8 +4,6 @@ import { Head, Link } from "aleph/react"
 export default function () {
   return <>
     <Head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>Liberchat</title>
       <meta name="description" content="Liberchat : 更新不要なリアルタイムチャットを最高峰のUI/UXで、Liberlunaが提供します。" />
       <link rel="icon" href="/assets/icon.svg" />
