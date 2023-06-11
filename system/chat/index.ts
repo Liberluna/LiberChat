d55b9b1f29453c5c795d7a28cc0a525e9006bcf5
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-11 09:07:55 +0900

    Update index.ts

--- system/chat/index.ts
+++ system/chat/index.ts
@@ -1,3 +1,14 @@
-export function from () {
+/**
+ * メッセージのType定義
+ */
+export interface Message {
+  text: string
+  user: string
+  room: string
+}
+/**
+ * 受信処理をする関数。
+ */
+export function from (msg: Message) {
   
 }
