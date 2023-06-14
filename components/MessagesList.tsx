87455ea80c727b0cd579c0facddb9bab7b0cb56f
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-14 20:58:16 +0900

    test

--- components/MessagesList.tsx
+++ components/MessagesList.tsx
@@ -6,6 +6,10 @@ export interface Props {
 
 export default function(props: Props){
   return props.messages.map((message) => {
-    return <div class="mt-5 ml-5">{message}</div>
+    return <>
+      <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <p class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{message}</p>
+      </div>
+    </>
   })
 }
