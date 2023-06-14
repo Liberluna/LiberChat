8fb8a16b5217e400ada8f74e4f509b14f41f618d
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-14 20:59:33 +0900

    Update MessagesList.tsx　調整

--- components/MessagesList.tsx
+++ components/MessagesList.tsx
@@ -8,7 +8,7 @@ export default function(props: Props){
   return props.messages.map((message) => {
     return <>
       <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-        <p class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{message}</p>
+        <p class="mb-2 font-bold tracking-tight text-gray-600 dark:text-white">{message}</p>
       </div>
     </>
   })
