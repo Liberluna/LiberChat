2d32f7d93c3b1f1d55c71c2a682f1e197fac7720
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-14 21:04:48 +0900

    Update MessagesList.tsx

--- components/MessagesList.tsx
+++ components/MessagesList.tsx
@@ -4,12 +4,19 @@ export interface Props {
   messages: Message[]
 }
 
-export default function(props: Props){
-  return props.messages.map((message) => {
-    return <>
-      <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-        <p class="mb-2 font-bold tracking-tight text-gray-600 dark:text-white">{message}</p>
-      </div>
-    </>
-  })
+export default function MessageList(props: Props) {
+  return (
+    <div>
+      {props.messages.map((message, index) => (
+        <div
+          msg={index}
+          className="block w-full max-w-sm p-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        >
+          <p className="mb-2 font-bold tracking-tight text-gray-600 dark:text-white">
+            {message}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
 }
