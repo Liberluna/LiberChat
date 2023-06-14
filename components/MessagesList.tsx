e4695526c03ed1f3f7cbadfd8c602436f1cbc9e7
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-14 21:58:55 +0900

    Update MessagesList.tsx

--- components/MessagesList.tsx
+++ components/MessagesList.tsx
@@ -10,7 +10,7 @@ export default function MessageList(props: Props) {
       {props.messages.map((message, index) => (
         <div
           key={index}
-          className="block w-full my-5 p-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+          className="block w-full my-4 p-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
         >
           <div className="mb-2 tracking-tight text-gray-600 dark:text-white">無名のLiberさん</div>
           <p className="mb-2 font-bold tracking-tight text-gray-800 dark:text-white">
