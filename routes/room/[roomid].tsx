20b7e4a5334511d806846fc39c270fa7d5d97c00
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-14 23:38:57 +0900

    Update [roomid].tsx

--- routes/room/[roomid].tsx
+++ routes/room/[roomid].tsx
@@ -4,11 +4,36 @@ import Room from "~/islands/Room.tsx"
 
 export default function (props: PageProps) {
   const { roomid } = props.params
+  const css = `
+  ::-webkit-scrollbar-track {
+  background-color: #f1f1f1;
+}
 
+::-webkit-scrollbar-thumb {
+  background-color: #888;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background-color: #555;
+}
+
+::-webkit-scrollbar-corner {
+  background-color: #f1f1f1;
+}
+
+::-webkit-scrollbar-resizer {
+  background-color: #f1f1f1;
+}
+
+  `
+  
   return <>
     <Head>
       <title>Room : {roomid} | Liberchat </title>
       <meta name="description" content="LiberChat : 更新不要なリアルタイムチャットを最高峰のUI/UXで、Liberlunaが提供します。" />
+      <style>
+        {css}
+      </style>
     </Head>
     <div className="bg-white dark:bg-gray-800 w-full h-screen overflow-x-hidden">
       <Room roomId={roomid}/>
