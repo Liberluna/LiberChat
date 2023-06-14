fafc2b878554720a3e570750b499367128302049
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-14 22:06:10 +0900

    Update MessagesList.tsx

--- components/MessagesList.tsx
+++ components/MessagesList.tsx
@@ -13,7 +13,7 @@ export default function MessageList(props: Props) {
           className="block w-full my-4 p-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
         >
           <div className="mb-2 tracking-tight text-gray-600 dark:text-white">無名のLiberさん</div>
-          <p className="mb-2 font-bold tracking-tight text-gray-800 dark:text-white">
+          <p className="mb-2 font-bold tracking-tight text-gray-800 dark:text-white break-words">
             {message}
           </p>
         </div>
