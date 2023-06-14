d356f4575eec2d6242e754ee824646626f10ea5a
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-14 22:25:41 +0900

    Update [roomid].tsx

--- routes/room/[roomid].tsx
+++ routes/room/[roomid].tsx
@@ -1,9 +1,13 @@
 import { PageProps } from "$fresh/server.ts"
+import { Head } from "$fresh/runtime.ts"
 import Room from "~/islands/Room.tsx"
 export default function (props: PageProps) {
   const { roomid } = props.params
 
   return <>
+    <Head>
+      <title>Liberchat room:{roomid}</title>
+    </Head>
     <div className="bg-white dark:bg-gray-800 w-full h-screen overflow-x-hidden">
       <Room roomId={roomid}/>
     </div>
