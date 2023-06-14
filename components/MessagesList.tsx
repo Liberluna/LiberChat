7d4d41e22456be8488be574c949cefb49ebf55e6
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-14 21:00:38 +0900

    Update MessagesList.tsx

--- components/MessagesList.tsx
+++ components/MessagesList.tsx
@@ -7,7 +7,7 @@ export interface Props {
 export default function(props: Props){
   return props.messages.map((message) => {
     return <>
-      <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+      <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <p class="mb-2 font-bold tracking-tight text-gray-600 dark:text-white">{message}</p>
       </div>
     </>
