f6a8481baa2ed5e171703eff6c1f40756f3ab59e
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-14 21:59:26 +0900

    Update Room.tsx

--- islands/Room.tsx
+++ islands/Room.tsx
@@ -55,7 +55,7 @@ export default class extends Component{
   render(props?: Readonly<Attributes & { children?: ComponentChildren; ref?: Ref<any> | undefined; }> | undefined, state?: Readonly<{}> | undefined, context?: any): ComponentChild {
     const inp = useRef<HTMLInputElement>(null)
     return <>
-      <div className="flex pl-4">
+      <div className="flex pl-4 my-3">
       <input ref={inp} placeholder="message" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
       <button onClick={() => {
               if (inp.current.value === "") {
