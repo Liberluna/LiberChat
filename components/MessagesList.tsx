04310e154844396183cf673946e74ba36afabba9
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-15 13:42:59 +0900

    Update MessagesList.tsx

--- components/MessagesList.tsx
+++ components/MessagesList.tsx
@@ -5,8 +5,10 @@ export interface Props {
 }
 
 export default function MessageList(props: Props) {
-  const max = 200
-  const username = "名無しのLiberさん"
+  const max = 200 //max
+  const username = "名無しのLiberさん" //ユーザーネーム機能を付けるまでの暫定 
+  const date = "x/x/x/xx:xx"; //暫定時間
+  //名前・時間はmessageにobjectとして
   
   return (
     <div>
@@ -15,7 +17,7 @@ export default function MessageList(props: Props) {
           key={index} 
           className="block w-full my-4 p-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
         >
-          <div className="mb-2 tracking-tight text-gray-600 dark:text-white">{username}</div>
+          <div className="mb-2 tracking-tight text-gray-600 dark:text-white">{username} {date}</div>
           <p className="mb-2 font-bold tracking-tight text-gray-800 dark:text-white break-words">
             {message.length > max ? message.slice(max) + "..." : message}
           </p>
