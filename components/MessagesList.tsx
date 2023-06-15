af7b0961f528b3083dd7ce32c02574ce3950f0ae
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-15 12:44:25 +0900

    文字数制限機能 とりあえず140文字

--- components/MessagesList.tsx
+++ components/MessagesList.tsx
@@ -5,16 +5,17 @@ export interface Props {
 }
 
 export default function MessageList(props: Props) {
+  const max = 140
   return (
     <div>
       {props.messages.map((message, index) => (
         <div
-          key={index}
+          key={index} 
           className="block w-full my-4 p-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
         >
           <div className="mb-2 tracking-tight text-gray-600 dark:text-white">無名のLiberさん</div>
           <p className="mb-2 font-bold tracking-tight text-gray-800 dark:text-white break-words">
-            {message}
+            {message.length > max ? message.slice(max) + "..." : message}
           </p>
         </div>
       ))}
