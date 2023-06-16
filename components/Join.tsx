2b0aff8562d2c18bcaa173147722e430e11e5077
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-16 21:08:01 +0900

    Create Join.tsx

---
+++ components/Join.tsx
@@ -0,0 +1,16 @@
+export default function () {
+
+  const join = () => {
+    let room = document.getElementById("room").velue;
+    location.href = "https://liberchat.deno.dev/room/" + room;
+  }
+
+  return <>
+    <div className="mx-10">
+      <div className="text-center text-2xl">LiberChat Join</div>
+      <label for="room">Room</label>
+      <input type="text" id="room" placeholder="main">
+        <button onClick="join">Join</button>
+    </div>
+  </>
+}
