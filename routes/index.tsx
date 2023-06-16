1d2d1cfd078ce0f8aed930b91a3cae22f443e936
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-16 21:52:39 +0900

    Update index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,48 +1,52 @@
-import { Head } from "$fresh/runtime.ts"
+import { Head } from "$fresh/runtime.ts";
 import { useState } from "preact/hooks";
 
-export default function () {
+export default function JoinPage() {
   const [room, setRoom] = useState("");
 
-     const join = () => {
-          window.location.href = "https://liberchat.deno.dev/room/" + room; //くっつける
-          window.console.log(room);
-     };
+  const join = () => {
+    window.location.href = "https://liberchat.deno.dev/room/" + room;
+    console.log(room);
+  };
 
-     const handleChange = (e) => {
-          setRoom(e.target.value); //roomと同期
-     };
-     
-     window.console.log("Welcome to Liberchat!"); //test
-  
-  return <>
-    <Head>
-      <title>Liberchat</title>
-      <meta name="description" content="LiberChat : 更新不要なリアルタイムチャットを最高峰のUI/UXで、Liberlunaが提供します。" />
-    </Head>
-    <div className="mx-10">
-      <div className="text-center text-2xl">LiberChat</div>
-      <div>LiberChatへようこそ！</div>
-      <div>LiberChatは、オープンソースのオンラインチャットサービスです。</div>
-      <div>
-        <div>さあ！始めよう！</div>
+  const handleChange = (e) => {
+    setRoom(e.target.value);
+  };
+
+  console.log("Welcome to Liberchat!");
+
+  return (
+    <>
+      <Head>
+        <title>Liberchat</title>
+        <meta
+          name="description"
+          content="LiberChat : 更新不要なリアルタイムチャットを最高峰のUI/UXで、Liberlunaが提供します。"
+        />
+      </Head>
+      <div className="mx-10">
+        <div className="text-center text-2xl">LiberChat</div>
+        <div>LiberChatへようこそ！</div>
+        <div>LiberChatは、オープンソースのオンラインチャットサービスです。</div>
+        <div>
+          <div>さあ！始めよう！</div>
+        </div>
+
+        <div className="text-center text-2xl">LiberChat Join</div>
+
+        <label htmlFor="room">Room</label>
+        <input
+          type="text"
+          id="room"
+          className="border"
+          placeholder="main"
+          value={room}
+          onChange={handleChange}
+        />
+        <button onClick={join} className="border">
+          Join
+        </button>
       </div>
-      
-      <div className="text-center text-2xl">LiberChat Join</div>
-      
-               <label htmlFor="room">Room</label>
-               <input
-                    type="text"
-                    id="room"
-                    className="border"
-                    placeholder="main"
-                    value={room}
-                    onChange={handleChange}
-               />
-               <button onClick={join} className="border">
-                    Join
-               </button>
-      
-    </div>
-  </>
+    </>
+  );
 }
