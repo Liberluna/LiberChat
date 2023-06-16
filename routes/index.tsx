1c8a2c6be57b95251bcb7fa313bb10061fd7f7d6
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-16 21:50:59 +0900

    Update index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,7 +1,20 @@
 import { Head } from "$fresh/runtime.ts"
-import Join from "~/islands/Join.tsx"
+import { useState } from "preact/hooks";
 
 export default function () {
+  const [room, setRoom] = useState("");
+
+     const join = () => {
+          window.location.href = "https://liberchat.deno.dev/room/" + room; //くっつける
+          window.console.log(room);
+     };
+
+     const handleChange = (e) => {
+          setRoom(e.target.value); //roomと同期
+     };
+     
+     window.console.log("Welcome to Liberchat!"); //test
+  
   return <>
     <Head>
       <title>Liberchat</title>
@@ -15,7 +28,21 @@ export default function () {
         <div>さあ！始めよう！</div>
       </div>
       
-      <Join />
+      <div className="text-center text-2xl">LiberChat Join</div>
+      
+               <label htmlFor="room">Room</label>
+               <input
+                    type="text"
+                    id="room"
+                    className="border"
+                    placeholder="main"
+                    value={room}
+                    onChange={handleChange}
+               />
+               <button onClick={join} className="border">
+                    Join
+               </button>
+      
     </div>
   </>
 }
