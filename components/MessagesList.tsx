3421c18784c660d65ca1358e016a42402cf6271a
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-17 01:26:04 +0900

    fix

--- components/MessagesList.tsx
+++ components/MessagesList.tsx
@@ -16,34 +16,34 @@ export default function MessageList(props: Props) {
       {props.messages.map((message, index) => {
         const dateText = dateFns.format(message.date, "HH:mm:ss yyyy/MM/dd");
 
-        if (message.room != roomHere) {
+        if (message.room !== roomHere) {
           return (
             <>
               <p className="hidden">other room's msg</p>
             </>
           ); //何も返さない
-        }
-
-        if (DoNotUseWords.includes(message.body)) {
-          for (let i = 0; i < DoNotUseWords.length; i++) {
-            message.body = message.body.replaceAll(DoNotUseWords[i], SysMsg);
+        } else {
+          if (DoNotUseWords.includes(message.body)) {
+            for (let i = 0; i < DoNotUseWords.length; i++) {
+              message.body = message.body.replaceAll(DoNotUseWords[i], SysMsg);
+            }
           }
-        }
 
-        return (
-          <div
-            key={index}
-            className="block w-full my-4 p-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-          >
-            <div className="mb-2 tracking-tight text-gray-600 dark:text-white flex gap-4">
-              <span>@{message.user}</span>
-              <span>{dateText}</span>
+          return (
+            <div
+              key={index}
+              className="block w-full my-4 p-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+            >
+              <div className="mb-2 tracking-tight text-gray-600 dark:text-white flex gap-4">
+                <span>@{message.user}</span>
+                <span>{dateText}</span>
+              </div>
+              <p className="mb-2 font-bold tracking-tight text-gray-800 dark:text-white break-words">
+                {message.body}
+              </p>
             </div>
-            <p className="mb-2 font-bold tracking-tight text-gray-800 dark:text-white break-words">
-              {message.body}
-            </p>
-          </div>
-        );
+          );
+        }
       })}
     </div>
   );
