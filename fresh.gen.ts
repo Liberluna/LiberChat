1bf44c30dafd6d7e8d1bf1169cd09dd75630c0a2
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-17 00:39:48 +0900

    管理者ぺージ追加

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,9 +5,10 @@
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/_404.tsx";
 import * as $1 from "./routes/api/joke.ts";
-import * as $2 from "./routes/index.tsx";
-import * as $3 from "./routes/room/[roomid].tsx";
-import * as $4 from "./routes/updates.tsx";
+import * as $2 from "./routes/dev347.tsx";
+import * as $3 from "./routes/index.tsx";
+import * as $4 from "./routes/room/[roomid].tsx";
+import * as $5 from "./routes/updates.tsx";
 import * as $$0 from "./islands/Join.tsx";
 import * as $$1 from "./islands/Room.tsx";
 
@@ -15,9 +16,10 @@ const manifest = {
   routes: {
     "./routes/_404.tsx": $0,
     "./routes/api/joke.ts": $1,
-    "./routes/index.tsx": $2,
-    "./routes/room/[roomid].tsx": $3,
-    "./routes/updates.tsx": $4,
+    "./routes/dev347.tsx": $2,
+    "./routes/index.tsx": $3,
+    "./routes/room/[roomid].tsx": $4,
+    "./routes/updates.tsx": $5,
   },
   islands: {
     "./islands/Join.tsx": $$0,
---
+++ routes/dev347.tsx
@@ -0,0 +1,7 @@
+//管理者機能
+
+export default function DevPage() {
+  return <>
+    <h1>管理ページ&ログページ テスト</h1>
+  </>
+}
