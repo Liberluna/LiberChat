ed40423dfb9f343aeb760cc7d069197eeb242134
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-16 22:46:49 +0900

    Update index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -11,7 +11,8 @@ export default function JoinPage() {
           content="LiberChat : 更新不要なリアルタイムチャットを最高峰のUI/UXで、Liberlunaが提供します。"
         />
       </Head>
-      <div className="mx-10">
+      <div className="mx-10 dark:bg-gray-100">
+        <img alt="LiberChat" src="https://github.com/Liberluna/L" className="mx-auto"/>
         <div className="text-center text-2xl">LiberChat</div>
         <div>LiberChatへようこそ！</div>
         <div>LiberChatは、オープンソースのオンラインチャットサービスです。</div>
