8869fabdcac9f4797192d0cfb7d1749d1a2d39b3
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-16 21:12:21 +0900

    Update Join.tsx

--- components/Join.tsx
+++ components/Join.tsx
@@ -9,8 +9,8 @@ export default function () {
     <div className="mx-10">
       <div className="text-center text-2xl">LiberChat Join</div>
       <label for="room">Room</label>
-      <input type="text" id="room" placeholder="main" />
-        <button onClick="join">Join</button>
+      <input type="text" id="room" className="border" placeholder="main" />
+        <button onClick="join()" className="border">Join</button>
     </div>
   </>
 }
