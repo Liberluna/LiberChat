922ee96c3cbebcbe6d8d34c70d957e570b8db223
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-17 00:52:24 +0900

    fix　伏字機能

--- components/MessagesList.tsx
+++ components/MessagesList.tsx
@@ -1,27 +1,27 @@
-import { type Message } from "~/core/chat/index.ts"
-import * as dateFns from "date-fns"
+import { type Message } from "~/core/chat/index.ts";
+import * as dateFns from "date-fns";
 
 export interface Props {
-  messages: Message[]
+  messages: Message[];
 }
 
 export default function MessageList(props: Props) {
-
   return (
     <div>
-      {
-        props.messages.map((message, index) => {
-          const dateText = dateFns.format(message.date, "HH:mm:ss yyyy/MM/dd")
-          const DoNotUseWords: string[] = ["唐澤貴洋殺す","ポチンチン"];
+      {props.messages.map((message, index) => {
+        const dateText = dateFns.format(message.date, "HH:mm:ss yyyy/MM/dd");
+        const DoNotUseWords: string[] = ["唐澤貴洋殺す", "ポチンチン"];
+        const SysMsg = "**伏字**"; //伏字にしたときに置き換えられる言葉
 
-          if(DoNotUseWords.includes(message.body)){
-            for (let i = 0; i < DoNotUseWords.length; i++){
-              message.body.replaceAll(DoNotUseWords[i], "");
-            }
+        if (DoNotUseWords.includes(message.body)) {
+          for (let i = 0; i < DoNotUseWords.length; i++) {
+            message.body = message.body.replaceAll(DoNotUseWords[i], SysMsg);
           }
-
-          return <div
-            key={index} 
+        }
+        
+        return (
+          <div
+            key={index}
             className="block w-full my-4 p-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
           >
             <div className="mb-2 tracking-tight text-gray-600 dark:text-white flex gap-4">
@@ -29,11 +29,11 @@ export default function MessageList(props: Props) {
               <span>{dateText}</span>
             </div>
             <p className="mb-2 font-bold tracking-tight text-gray-800 dark:text-white break-words">
-              { message.body }
+              {message.body}
             </p>
           </div>
-        })
-      }
+        );
+      })}
     </div>
   );
 }
