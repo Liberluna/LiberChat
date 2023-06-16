0deb9f5c38fa9b2a92bd4c1c797f03a877eed35f
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-16 21:26:08 +0900

    Update Join.tsx

--- components/Join.tsx
+++ components/Join.tsx
@@ -1,34 +1,34 @@
 import React, { useState } from "react";
 
 export default function Join() {
-const [room, setRoom] = useState("");
+     const [room, setRoom] = useState("");
 
-const join = () => {
-window.location.href = "https://liberchat.deno.dev/room/" + room;
-};
+     const join = () => {
+          window.location.href = "https://liberchat.deno.dev/room/" + room;
+     };
 
-const handleChange = (e) => {
-setRoom(e.target.value);
-};
+     const handleChange = (e) => {
+          setRoom(e.target.value);
+     };
 
-return (
-<div className="mx-10">
-<div className="text-center text-2xl">LiberChat Join</div>
-<label htmlFor="room">Room</label>
-<input
-     type="text"
-     id="room"
-     className="border"
-     placeholder="main"
-     value={room}
-     onChange={handleChange}
-   />
-<button onClick={join} className="border">
-Join
-</button>
-</div>
-);
-}
+     return (
+          <div>
+               <div className="text-center text-2xl">LiberChat Join</div>
+               <label htmlFor="room">Room</label>
+               <input
+                    type="text"
+                    id="room"
+                    className="border"
+                    placeholder="main"
+                    value={room}
+                    onChange={handleChange}
+               />
+               <button onClick={join} className="border">
+                    Join
+               </button>
+          </div>
+          );
+     }
 
 
 
