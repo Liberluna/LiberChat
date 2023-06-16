015227852dff32bde1350bf08c5e5b544d961589
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-16 21:58:18 +0900

    Create Join.tsx

---
+++ islands/Join.tsx
@@ -0,0 +1,32 @@
+
+import { useState } from "preact/hooks";
+
+export default function Join() {
+     const [room, setRoom] = useState("");
+
+     const join = () => {
+          window.location.href = "https://liberchat.deno.dev/room/" + room; //くっつける
+     };
+
+     const handleChange = (e) => {
+          setRoom(e.target.value); //roomと同期
+     };
+
+     return (
+          <div>
+               <div className="text-center text-2xl">LiberChat Join</div>
+               <label htmlFor="room">Room</label>
+               <input
+                    type="text"
+                    id="room"
+                    className="border"
+                    placeholder="main"
+                    value={room}
+                    onChange={handleChange}
+               />
+               <button onClick={join} className="border">
+                    Join
+               </button>
+          </div>
+          );
+     }
