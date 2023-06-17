7a0cc4b4dfc8d65d92b1aa305c19a485e8657b64
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-17 13:39:11 +0900

    dev page test

---
+++ routes/pages/devs.tsx
@@ -0,0 +1,30 @@
+import { Head } from "$fresh/runtime.ts"
+import Layout from "~/components/Layout.tsx"
+
+export default function JoinPage() {
+  return (
+    <>
+      <Head>
+        <title>Liberchat Developer</title>
+        <meta
+          name="description"
+          content="LiberChat : 更新不要なリアルタイムチャットを最高峰のUI/UXで、Liberlunaが提供します。"
+        />
+      </Head>
+      <Layout>
+        <h1>LiberChat Developers</h1>
+        <div className="mt-5 flex flex-col gap-5 md:flex-row md:gap-10">
+          <div>
+            <img src="https://avatars.githubusercontent.com/u/121654029" alt="DevIcon" />
+            <div>
+              Name
+            </div>
+            <div className="">
+              Description
+            </div>
+          </div>
+        </div>
+      </Layout>
+    </>
+  );
+}
