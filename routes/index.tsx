7cbc664402091915390fc99ad063229ce134758e
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-17 11:48:43 +0900

    Update index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,5 +1,6 @@
-import { Head } from "$fresh/runtime.ts";
-import Join from "~/islands/Join.tsx";
+import { Head } from "$fresh/runtime.ts"
+import Join from "~/islands/Join.tsx"
+import Layout from "~/components/Layout.tsx"
 
 export default function JoinPage() {
   return (
@@ -11,8 +12,7 @@ export default function JoinPage() {
           content="LiberChat : 更新不要なリアルタイムチャットを最高峰のUI/UXで、Liberlunaが提供します。"
         />
       </Head>
-      <div className="bg-gray-800 w-full h-screen text-white flex flex-col">
-        <h1 className="hidden">LiberChat</h1>
+      <Layout>
         <img
           src="https://raw.githubusercontent.com/Liberluna/LiberChat/main/assets/concept.svg"
           alt="Logo"
@@ -31,7 +31,7 @@ export default function JoinPage() {
         <div className="text-center mt-5">
           <Join />
         </div>
-      </div>
+      </Layout>
     </>
   );
 }
