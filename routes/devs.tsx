8a792ee02c96e08ac76e47d10a12500c4a78194f
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-17 14:00:15 +0900

    fix test

--- routes/devs.tsx
+++ routes/devs.tsx
@@ -1,9 +1,8 @@
-import { Head } from "$fresh/runtime.ts"
-import Layout from "~/components/Layout.tsx"
-import Devs from "~/components/config/developer.ts"
+import { Head } from "$fresh/runtime.ts";
+import Layout from "~/components/Layout.tsx";
+import Devs from "~/components/config/developer.ts";
 
 export default function DevPage() {
-
   return (
     <>
       <Head>
@@ -17,16 +16,13 @@ export default function DevPage() {
         <h1>LiberChat Developers</h1>
         <div className="mt-5 flex flex-col gap-5 md:flex-row md:gap-10">
           <div>
-            <img src="https://avatars.githubusercontent.com/u/121654029" alt="DevIcon" />
-            <div>
-              Name
-            </div>
-            <div className="">
-              Description
-            </div>
+            <img
+              src="https://avatars.githubusercontent.com/u/121654029"
+              alt="DevIcon"
+            />
+            <div>Name</div>
+            <div className="">Description</div>
           </div>
-
-          { Devs }
         </div>
       </Layout>
     </>
