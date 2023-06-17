3b9078e660ee7c465c85a201b2cf539e5ec044ce
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-17 12:13:33 +0900

    Add header

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,8 @@
+import Header from "./Header.tsx"
+
 export default function (props) {
   return <>
+    <Header />
     <div className="dark:bg-gray-800 dark:text-gray-50">
       <div className="mx-10">
         { props.children }
