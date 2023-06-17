1d762e83b5fe8173c3ca8b2c0005453aab4aa5d1
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-17 12:23:36 +0900

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function () {
+  return <>
+    <footer>
+      <div>&copy; 2023 LiberChat Team. MIT LICENSED. All Rights Reserved.</div>
+      <div>Producted by <a href="https://liberluna.github.io" className="cursor-pointer underline hover:no-underline">LiberLuna</a></div>
+    </footer>
+  </>
+}
