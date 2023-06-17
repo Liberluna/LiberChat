b8bb8d84c51e906b307a11ca8b57c0a81c9f92ef
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-17 15:16:28 +0900

    Update and rename developer.ts to developers.ts

---
+++ components/config/developers.ts
@@ -0,0 +1,23 @@
+interface Developer {
+  icon: string
+  name: string
+  desc: string
+  link: string
+}
+
+const Devs: Developer[] = [
+  {
+    icon: "https://github.com/nakasyou.png",
+    name: "nakasyou",
+    desc: "LiberChatの開発指揮者、メイン開発者。主にバックエンドを担当。",
+    link: "https://nakasyou.github.io"
+  },
+  {
+    icon: "https://avatars.githubusercontent.com/u/121654029",
+    name: "ame . x",
+    desc: "色々してる訳ではないが色々してる",
+    link: "https://github.com/edamame-x"
+  },
+]
+
+export default Devs
