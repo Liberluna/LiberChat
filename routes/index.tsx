7ea6eb01463e75b294ba01c2a209553ded121d07
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-17 12:11:07 +0900

    Update index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -22,12 +22,12 @@ export default function JoinPage() {
               className="h-80 mx-auto text-center"
             />
           </div>
-          <div>LiberChat 0.1.0 Alpha</div>
+          <div className="leading-loose">LiberChat 0.1.0 Alpha</div>
           <div>
             <div className="text-2xl">About</div>
             <div>LiberChatは、自由で開かれたチャットサイトです。</div>
             <div>
-              13歳未満使用禁止、16歳未満使用禁止、、、<br />
+              13歳未満使用禁止、16歳未満使用禁止...<br />
               ネット上のサイトには、さまざまな年齢制限があります。
               しかし、LiberChatには年齢制限はありません。
               誰でも、年齢性別関係なく、公平に話せるチャットサイトです。
@@ -35,7 +35,9 @@ export default function JoinPage() {
           </div>
         </div>
         <div className="text-center mt-5">
+          <div className="text-2xl">さあ！始めよう！</div>
           <Join />
+          <div></div>
         </div>
       </Layout>
     </>
