9f78cdecce091c15513fe6a369632b9a325fd6bf
nakasyou <syoutarou1103@outlook.jp>
2023-06-17 10:38:29 +0000

    Revert "ok"
    This reverts commit 9db4ecf6be79610f283df32e1ef584cf6bbdf9fc.

--- islands/Room.tsx
+++ islands/Room.tsx
@@ -62,8 +62,6 @@ export default class extends Component {
 
     const AddRes = (message: Message) => {
       //inputBoxに追加
-      if (!inp.current) return;
-      inp.current.value += message;
     }
     return (
       <>
