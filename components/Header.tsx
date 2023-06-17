174e129975212c5f7b407149a43d2b5020d93f93
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-17 13:50:01 +0900

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,10 @@
+import Logo from "./Logo.tsx"
+
 export default function () {
   return <header className="sticky top-0 w-full">
     <div className="drop-shadow-lg flex flex-wrap">
       <div>
+        <Logo />
         <img src="https://raw.githubusercontent.com/Liberluna/LiberChat/main/assets/logo.svg" alt="LiberChat" className="h-10" />
       </div>
     </div>
