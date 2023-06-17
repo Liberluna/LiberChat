a81c901094f5dcedd53e1e138310f86cf302b81a
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-17 15:59:44 +0900

    Update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,12 +1,13 @@
-export default function () {
+export default function ({ width, height } : { width: number, height: number, }) {
   return (
     <>
       <a href="/">
-        <img src="/logo/light.svg" className="dark:hidden" alt="LiberChat" />
+        <img src="/logo/light.svg" className="dark:hidden" alt="LiberChat" width={width} height={height}/>
         <img
           src="/logo/dark.svg"
           className="hidden dark:inline"
           alt="LiberChat"
+          width={width} height={height}
         />
       </a>
     </>
