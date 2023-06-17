576b343cdaf0b513c8b80e190d7eb2d120b6c527
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-17 13:02:23 +0900

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,7 +2,41 @@ import Header from "./Header.tsx"
 import Footer from "./Footer.tsx"
 
 export default function (props) {
+  
+    const css = `
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+::-webkit-scrollbar-track {
+  background-color: #333;
+  border-radius: 8px;
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: #888;
+  border-radius: 8px;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background-color: #aaa;
+}
+
+::-webkit-scrollbar-corner {
+  background-color: #333;
+  border-radius: 8px;
+  opacity: 0.8;
+}
+
+::-webkit-scrollbar-resizer {
+  background-color: #333;
+  border-radius: 8px;
+  opacity: 0.8;
+}
+  `
+  
   return <>
+    <style>{css}</style>
     <Header />
     <div className="dark:bg-gray-800 dark:text-gray-50">
       <div className="mx-10">
