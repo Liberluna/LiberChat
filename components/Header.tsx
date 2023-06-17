dd6eae957005e167699eab8fbb55402b5dd12c85
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-17 14:02:13 +0900

    Logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ export default function () {
   return <header className="sticky top-0 w-full">
     <div className="drop-shadow-lg flex flex-wrap">
       <div>
-        <img src="https://raw.githubusercontent.com/Liberluna/LiberChat/main/assets/logo.svg" alt="LiberChat" className="h-10" />
+        <Logo />
       </div>
     </div>
   </header>
