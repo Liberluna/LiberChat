309b98d8e5950fa7e4e030fa11d1006c8009aa86
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-17 15:34:41 +0900

    shuffle

--- components/config/developers.ts
+++ components/config/developers.ts
@@ -1,23 +1,35 @@
 interface Developer {
-  icon: string
-  name: string
-  desc: string
-  link: string
+  icon: string;
+  name: string;
+  desc: string;
+  link: string;
 }
 
-const Devs: Developer[] = [
+let Devs: Developer[] = [
   {
     icon: "https://github.com/nakasyou.png",
     name: "nakasyou",
     desc: "LiberChatの開発指揮者、メイン開発者。主にバックエンドを担当。",
-    link: "https://nakasyou.github.io"
+    link: "https://nakasyou.github.io",
   },
   {
     icon: "https://avatars.githubusercontent.com/u/121654029",
     name: "ame . x",
     desc: "色々してる訳ではないが色々してる",
-    link: "https://github.com/edamame-x"
+    link: "https://github.com/edamame-x",
   },
-]
+];
 
-export default Devs
+//shuffle
+
+function shuffle<T>(array: T[]): T[] {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+Devs = shuffle(Devs);
+
+export default Devs;
--- fresh.gen.ts
+++ fresh.gen.ts
@@ -11,8 +11,8 @@ import * as $4 from "./routes/index.tsx";
 import * as $5 from "./routes/room/[roomid].tsx";
 import * as $6 from "./routes/updates.tsx";
 import * as $$0 from "./islands/BeautifulFooter.tsx";
-import * as $$2 from "./islands/Join.tsx";
-import * as $$3 from "./islands/Room.tsx";
+import * as $$1 from "./islands/Join.tsx";
+import * as $$2 from "./islands/Room.tsx";
 
 const manifest = {
   routes: {
@@ -26,8 +26,8 @@ const manifest = {
   },
   islands: {
     "./islands/BeautifulFooter.tsx": $$0,
-    "./islands/Join.tsx": $$2,
-    "./islands/Room.tsx": $$3,
+    "./islands/Join.tsx": $$1,
+    "./islands/Room.tsx": $$2,
   },
   baseUrl: import.meta.url,
   config,
