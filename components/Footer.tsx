2faace93fbf218fa70f96476129a899ce2c94796
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-17 15:48:46 +0900

    Revert

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,41 @@
-import Footers from "~/islands/BeautifulFooter.tsx";
-
-export default function Footer() {
-  //JSを使えるようにislandsから移植
-  return <Footers />;
+export default function () {
+  return (
+    <>
+      <footer className="bg-gray-800 text-white pt-20">
+        <div className="flex flex-wrap bg-gray-700">
+          <div>
+            <div className="font-bold uppercase opacity-50 text-center w-screen">
+              Links
+            </div>
+            <div className="text-center grid grid-rows-2 grid-cols-2 gap-2">
+              <a
+                href="/updates"
+                className="cursor-pointer hover:underline mx-5"
+              >
+                Release notes
+              </a>
+              <a
+                href="https://github.com/LiberLuna/LiberChat"
+                className="cursor-pointer hover:underline mx-5"
+              >
+                Repository
+              </a>
+            </div>
+          </div>
+          <div className="mx-2 mt-5 text-center w-screen">
+            &copy; 2023 LiberChat Team. MIT LICENSED. All Rights Reserved.
+          </div>
+          <div className="mx-2 text-center w-screen">
+            Producted by{" "}
+            <a
+              href="https://liberluna.github.io"
+              className="cursor-pointer underline hover:no-underline"
+            >
+              LiberLuna
+            </a>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 }
