2b54b41ecae1a4e784658880f467c3a3e985cde9
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-17 14:10:29 +0900

    fix

--- components/config/developer.ts
+++ components/config/developer.ts
@@ -15,18 +15,5 @@ const Devs: Devs[] = [
 ]
 
 
-//ここで組み立て
-
-/*
-<div className="text-center w-30">
-            <img
-              src="https://avatars.githubusercontent.com/u/121654029"
-              alt="DevIcon"
-              className="rounded-full w-20 h-20 mt-5 mx-auto"
-            />
-            <div className="mt-5 text-xl">Name</div>
-            <div className="mt-2 text-sm word-break">Description</div>
-          </div>
-*/
 
 export default Devs
