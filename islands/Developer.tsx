4905d2b82f68f037873dd8bdc54e82a3432737d0
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-17 15:09:21 +0900

    add test Link

--- islands/Developer.tsx
+++ islands/Developer.tsx
@@ -2,11 +2,11 @@ import Devs from "~/components/config/developer.ts";
 
 export default function Developers() {
   const devElements = Devs.map((dev, index) => (
-    <div className="text-center w-30" key={index}>
+    <a className="text-center w-30" key={index} href={dev.Link}>
       <img src={dev.Icon} alt="DevIcon" className="rounded-full w-20 h-20 mt-5 mx-auto" />
       <div className="mt-5 text-xl">{dev.Name}</div>
       <div className="mt-2 mb-3 text-sm word-break">{dev.Desc}</div>
-    </div>
+    </a>
   ));
 
   return <>{devElements}</>;
