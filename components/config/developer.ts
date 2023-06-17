cf92c7e0f7e0df09301e255ebb8fdbcbd652fbbb
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-17 15:12:55 +0900

    Update developer.ts

--- components/config/developer.ts
+++ components/config/developer.ts
@@ -7,17 +7,17 @@ interface Devs {
 
 const Devs: Devs[] = [
     {
+        Icon: "https://github.com/nakasyou.png",
+        Name: "nakasyou",
+        Desc: "未定",
+        Link: "https://nakasyou.github.io"
+    },
+    {
         Icon: "https://avatars.githubusercontent.com/u/121654029",
         Name: "ame . x",
         Desc: "色々してる訳ではないが色々してる",
         Link: "https://github.com/edamame-x" //後で実装
     },
-    {
-        Icon: "https://avatars.githubusercontent.com/u/79000684?s=64&v=4",
-        Name: "nakasyou",
-        Desc: "未定",
-        Link: "https://github.com/nakasyou"
-    }
 ]
 
 
