db82fa7f3783e166c987da64a48efa6d4eebc82e
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-17 12:31:46 +0900

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,16 @@
 export default function () {
   return <>
-    <footer>
-      <div>&copy; 2023 LiberChat Team. MIT LICENSED. All Rights Reserved.</div>
-      <div>Producted by <a href="https://liberluna.github.io" className="cursor-pointer underline hover:no-underline">LiberLuna</a></div>
+    <footer className="bg-base-200 text-white">
+      <div className="flex flex-wrap">
+        <div>
+          <div className="font-bold uppercase opacity-50">Links</div>
+          <div></div>
+        </div>
+      </div>
+      <div className="text-center">
+        <div>&copy; 2023 LiberChat Team. MIT LICENSED. All Rights Reserved.</div>
+        <div>Producted by <a href="https://liberluna.github.io" className="cursor-pointer underline hover:no-underline">LiberLuna</a></div>
+      </div>
     </footer>
   </>
 }
