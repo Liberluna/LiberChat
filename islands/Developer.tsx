919c171adb69086a4fc5dd3be14e09fc1f176599
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-17 14:17:34 +0900

    fix

--- islands/Developer.tsx
+++ islands/Developer.tsx
@@ -1,23 +1,13 @@
 import Devs from "~/components/config/developer.ts";
 
 export default function Developers() {
+  const devElements = Devs.map((dev, index) => (
+    <div className="text-center w-30" key={index}>
+      <img src={dev.Icon} alt="DevIcon" className="rounded-full w-20 h-20 mt-5 mx-auto" />
+      <div className="mt-5 text-xl">{dev.Name}</div>
+      <div className="mt-2 text-sm word-break">{dev.Desc}</div>
+    </div>
+  ));
 
-  let dev_html:string = "";
-
-  for (let i = 0;i < Devs.length;i++) {
-    dev_html += `<div className="text-center w-30">`;
-    dev_html += `<img src="`;
-    dev_html += Devs[i].Icon;
-    dev_html += `" alt="DevIcon" className="rounded-full w-20 h-20 mt-5 mx-auto" />`;
-    dev_html += `<div className="mt-5 text-xl">`;
-    dev_html += Devs[i].Name;
-    dev_html += `</div>`;
-    dev_html += `<div className="mt-2 text-sm word-break">`;
-    dev_html += Devs[i].Desc;
-    dev_html += `</div>`;
-  }
-
-  return <>
-    {dev_html}
-  </>
+  return <>{devElements}</>;
 }
