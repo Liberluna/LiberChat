a69ac3ec2656733c4516e9578ed172ed5c41465d
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-17 14:08:59 +0900

    f

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,8 @@
 import Logo from "./Logo.tsx"
 
 export default function () {
-  return <header className="sticky bg-slate-50 dark:bg-emerald-950 top-0 w-full">
+  //見栄えをどうにかしてくれ・・・
+  return <header className="sticky bg-slate-50  dark:bg-emerald-950 top-0 w-full">
     <div className="drop-shadow-lg flex flex-wrap">
       <div>
         <Logo />
