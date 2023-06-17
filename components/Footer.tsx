7ad9af9403c336701149c138859a95d356a2023c
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-17 12:56:34 +0900

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,14 @@
 export default function () {
   return <>
     <footer className="bg-gray-800 text-white pt-20">
-      <div className="flex flex-wrap">
+      <div className="flex flex-wrap bg-gray-700">
         <div>
           <div className="font-bold uppercase opacity-50">Links</div>
           <div></div>
         </div>
       </div>
-      <div className="text-center">
-        <div>&copy; 2023 LiberChat Team. MIT LICENSED. All Rights Reserved.</div>
+      <div className="text-center bg-gray-700">
+        <div>&copy; 2023 LiberCht Team. MIT LICENSED. All Rights Reserved.</div>
         <div>Producted by <a href="https://liberluna.github.io" className="cursor-pointer underline hover:no-underline">LiberLuna</a></div>
       </div>
     </footer>
