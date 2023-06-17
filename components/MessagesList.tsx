0cd74828b02022f70b06ff19f50fbc7067318975
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-17 20:29:42 +0900

    入室メッセージ

--- components/MessagesList.tsx
+++ components/MessagesList.tsx
@@ -7,37 +7,39 @@ export interface Props {
   messages: Message[];
 }
 
-export default function MessageList(props: Props) {
-  
+export default function MessageList(props: Props) { 
   return (
     <div>
-      {props.messages.map((message, index) => {
-        const dateText = dateFns.format(message.date, "HH:mm:ss yyyy/MM/dd");
-        const MsgTripID = EasyHash(message.date + message.user + message.body); //識別ID  ここ消さんでな リプライ用
+      {
+        props.messages.map((message, index) => {
+          const dateText = dateFns.format(message.date, "HH:mm:ss yyyy/MM/dd");
 
-        if (DoNotUseWords.includes(message.body)) {
-          for (let i = 0; i < DoNotUseWords.length; i++) {
-            message.body = message.body.replaceAll(DoNotUseWords[i], SysMsg);
-          }
-        }
-      
-
-        return (
-          <div
-            key={index}
-            msg-trip={MsgTripID}
-            className="block w-full my-4 p-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-          >
-            <div className="mb-2 tracking-tight text-gray-600 dark:text-white flex gap-4">
-              <span>{message.user}</span>
-              <span>{dateText}</span>
+          if(message.type === "enter"){
+            // 誰かが入室
+            return <div class="">
+              <span>dateText</span>
+              <span>@{ message.user }</span>
+              <span>が入室しました。</span>
             </div>
-            <p className="mb-2 font-bold tracking-tight text-gray-800 dark:text-white break-words">
-              {message.body}
-            </p>
-          </div>
-        );
-      })}
+          }
+
+          
+          const MsgTripID = EasyHash(message.date + message.user + message.body); //識別ID  ここ消さんでな リプライ用
+          return <div
+              key={index}
+              msg-trip={MsgTripID}
+              className="block w-full my-4 p-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+            >
+              <div className="mb-2 tracking-tight text-gray-600 dark:text-white flex gap-4">
+                <span>{message.user}</span>
+                <span>{dateText}</span>
+              </div>
+              <p className="mb-2 font-bold tracking-tight text-gray-800 dark:text-white break-words">
+                {message.body}
+              </p>
+            </div>
+        })
+      }
     </div>
   );
 }
