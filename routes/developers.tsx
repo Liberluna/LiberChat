a08c250deeb4c97dc691a9ceb3e6dd4421c1c3f3
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-17 15:20:43 +0900

    Update developers.tsx

--- routes/developers.tsx
+++ routes/developers.tsx
@@ -15,13 +15,13 @@ export default function DevPage() {
       <Layout>
         <h1 className="text-4xl font-bold text-center">LiberChat Developers</h1>
         <div className="mt-5 flex flex-col gap-5 md:flex-row md:gap-10">
-          developers.map((dev, index) => (
+          {developers.map((dev, index) => (
             <a className="text-center w-30" key={index} href={dev.link}>
               <img src={dev.icon} alt="DevIcon" className="rounded-full w-20 h-20 mt-5 mx-auto" />
               <div className="mt-5 text-xl">{dev.name}</div>
               <div className="mt-2 mb-3 text-sm word-break">{dev.desc}</div>
             </a>
-          ))
+          ))}
         </div>
       </Layout>
     </>
