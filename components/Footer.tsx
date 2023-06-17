f017c77e4d0e0591ce9c2ee11c9635ea92a5eca4
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-17 14:21:39 +0900

    fix css

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,23 +1,17 @@
-import Link from "libercomp/Link.tsx"
+import Link from "libercomp/Link.tsx";
 
 export default function () {
   return (
     <>
-      <footer className="bg-gray-800 text-white pt-20 pb-5">
+      <footer className="bg-gray-800 text-white pt-50">
         <div className="flex flex-wrap bg-gray-700">
           <div>
             <div className="font-bold uppercase opacity-50 text-center w-screen">
               Links
             </div>
             <div className="text-center grid grid-rows-2 grid-cols-2 gap-2">
-              <Link
-                to="/updates"
-              >Release notes</Link>
-              <Link
-                to="https://github.com/LiberLuna/LiberChat"
-              >
-                GitHub
-              </Link>
+              <Link to="/updates">Release notes</Link>
+              <Link to="https://github.com/LiberLuna/LiberChat">GitHub</Link>
             </div>
           </div>
           <div className="mx-2 mt-5 text-center w-screen">
