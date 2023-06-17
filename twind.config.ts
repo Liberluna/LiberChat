6f261a75c929fb4b698c7d159f5f24e4a417571c
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-17 12:57:05 +0900

    Update twind.config.ts

--- twind.config.ts
+++ twind.config.ts
@@ -5,6 +5,9 @@ import presetTailwind from "https://esm.sh/@twind/preset-tailwind@1.1.4"
 export default {
   ...defineConfig({
     presets: [presetTailwind() as Preset],
+    plugins: {
+      red: `text-red-700`
+    }
   }),
   selfURL: import.meta.url,
 } as Options;
