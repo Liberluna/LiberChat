50335a951dabf929440447017e94dda34ba301c3
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-06-17 14:04:43 +0900

    fix css

--- routes/devs.tsx
+++ routes/devs.tsx
@@ -15,11 +15,11 @@ export default function DevPage() {
       <Layout>
         <h1 className="text-4xl font-bold">LiberChat Developers</h1>
         <div className="mt-5 flex flex-col gap-5 md:flex-row md:gap-10">
-          <div className="text-center w-60">
+          <div className="text-center w-30">
             <img
               src="https://avatars.githubusercontent.com/u/121654029"
               alt="DevIcon"
-              className="rounded-full w-36 h-36 mt-5"
+              className="rounded-full w-20 h-20 mt-5 mx-auto"
             />
             <div className="mt-5 text-xl">Name</div>
             <div className="mt-2 text-lg word-break">Description</div>
