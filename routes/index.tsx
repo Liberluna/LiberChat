c28585f56dc7f0edf44aa704d6f9d59638065ebc
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-17 19:58:06 +0900

    @tabler/icons-preact

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,7 +1,7 @@
 import { Head } from "$fresh/runtime.ts"
 import Join from "~/islands/Join.tsx"
 import Layout from "~/components/Layout.tsx"
-import { IconAward } from 'https://esm.sh/@tabler/icons-react'
+import { IconAward } from 'https://esm.sh/@tabler/icons-preact'
 
 export default function JoinPage() {
   return (
@@ -39,12 +39,6 @@ export default function JoinPage() {
           <Join />
           <div>Room IDを入力してください。同じRoom Idの人と話すことができます。何も入力しないで送信することでメインの部屋に入れます。</div>
         </div>
-<IconAward 
-    size={36} // set custom `width` and `height`
-    color="red" // set `stroke` color
-    stroke={3}  // set `stroke-width`
-    strokeLinejoin="miter" // override other SVG props
-  />
       </Layout>
     </>
   );
