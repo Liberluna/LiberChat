b2724a1300d14486a379ced876571faf1e71db6b
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-18 13:30:31 +0900

    disabled

--- islands/Room.tsx
+++ islands/Room.tsx
@@ -120,6 +120,7 @@ export default class extends Component {
                 sendMessage()
               }}
               class="mx-5 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
+              disabled={inp.current?.value === ""}
             >
               <IconSend />
             </button>
