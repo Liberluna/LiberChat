3d34259e13be3ee3bc66561696e932deb7fb66bc
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-18 13:22:46 +0900

    onleydown

--- islands/Room.tsx
+++ islands/Room.tsx
@@ -91,6 +91,9 @@ export default class extends Component {
             <input
               ref={inp}
               placeholder="message"
+              onKeyDown={(e)=>{
+                alert(0)
+              }}
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             />
             <button
