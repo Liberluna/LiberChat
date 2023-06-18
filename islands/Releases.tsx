0bb95347cb992c600e3a1c60e42835d2378c87b5
nakasyou <syoutarou1103@outlook.jp>
2023-06-18 18:37:31 +0900

    fix

--- islands/Releases.tsx
+++ islands/Releases.tsx
@@ -1,6 +1,11 @@
 import { useState, useEffect } from "preact/hooks"
 import Markdown from "~/components/Markdown.tsx"
 
+interface Release {
+  name: string
+  body: string
+  html_url: string
+}
 export default function () {
   const [ releases, setReleases ] = useState(["Now loding..."])
   
@@ -11,21 +16,32 @@ export default function () {
       if(!Array.isArray(json)){
         alert("Error. リリースの取得に失敗しました。")
       }
-      setReleases(json)
+      setReleases(json.reverse())
     })()
   }, [])
   return <>
     {
-      releases.map(release=>{
+      releases.map((release: Release | string)=>{
         if(typeof release === "string"){
           // Now loding
           return <p>{ release }</p>
         }
-        let { name, body } = release
+        let { name, body, html_url } = release
         body = body.replaceAll("\r\n", "\n")
-         // .replace(/^## /g, "### ")
-        return <div>
-          <h2 class="text-3xl">{ name }</h2>
+        body = body.replace(/@[a-zA-z0-9]+/g, (user: string)=>{
+          return `[${user}](https://github.com/${user.slice(1)})`
+        })
+        body = body.replaceAll(/## /g, "### ")
+        body = body.replace(/https:\/\/github.com\/Liberluna\/LiberChat\/pull\/[0-9]+/g, (url: string)=>{
+          const num = url.match(/[0-9]+/)
+          return `[#${num}](${url})`
+        })
+        return <div class="border p-5 m-10 rounded">
+          <div class="text-3xl underline">
+            <a href={html_url}>
+              <span>{ name }</span>
+            </a>
+          </div>
           <div>
             <Markdown>{ body }</Markdown>
           </div>
