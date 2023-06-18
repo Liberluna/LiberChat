a851f4760b453cb4075347de90ff316274f1cef9
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-18 10:52:11 +0900

    overflow-y-scroll

--- islands/Room.tsx
+++ islands/Room.tsx
@@ -76,7 +76,7 @@ export default class extends Component {
     
     return (
       <>
-        <div class="relative w-full h-full" ref={refBox}>
+        <div class="relative w-full h-full overflow-y-scroll" ref={refBox}>
           <div className="top-0 left-0 right-0 min-h-screen">
             <MessagesList messages={this.state.messages} reply={reply} />
           </div>
