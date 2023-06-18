e729a1b4e4a425898710594a9e61d963d6292137
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-18 14:03:24 +0900

    json

--- islands/Releases.tsx
+++ islands/Releases.tsx
@@ -1,17 +1,22 @@
 import { useState, useEffect } from "preact/hooks"
 
 export default function () {
-  const [ releases, setReleases ] = useState([])
+  const [ releases, setReleases ] = useState(["Now loding..."])
 
   useEffect(()=>{
     (async()=>{
-      setReleases(["a","b"])
+      const releaseRes: Respobse = await fetch("https://api.github.com/repos/liberluna/liberchat/releases")
+      const json = await releaseRes.json()
+      if(!Array.isArray(json)){
+        alert("Error. リリースの取得に失敗しました。")
+      }
+      setReleases(json)
     })()
   }, [])
   return <>
     {
       releases.map(release=>{
-        return <p>{release}</p>
+        return <p>{JSON.stringify(release)}</p>
       })
     }
   </>
