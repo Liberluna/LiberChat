c8430e831b76d661d594633091355f17180ada54
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-18 18:41:28 +0900

    Layout

--- routes/updates.tsx
+++ routes/updates.tsx
@@ -1,6 +1,7 @@
 import Markdown from "~/components/Markdown.tsx"
 import { Head } from "$fresh/runtime.ts"
 import Releases from "~/islands/Releases.tsx"
+import Layout from "~/components/Layout.tsx"
 
 export default function () {
   return <>
@@ -8,15 +9,8 @@ export default function () {
       <title>Liberchat</title>
       <meta name="description" content="LiberChat : 更新不要なリアルタイムチャットを最高峰のUI/UXで、Liberlunaが提供します。" />
     </Head>
-    <div>
+    <Layout>
       <Releases />
-    </div>
-    <Markdown>{`# Updates
-LiberChatのアップデート
-### v0.1.0 alpha-1 (2026-06-16) Hydrogen
-チャット機能の追加。
-### v0.1.1 alpha-2 (2026-06-17) 
-簡易的なユーザーネーム機能 大幅なUI改善 伏字機能 fix etc
-    `}</Markdown>
-    </>
+    </Layout>
+  </>
 }
