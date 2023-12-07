500c090bcbc9812de9e2e036d7513c5a62930fae
GitHub <noreply@github.com>
2023-12-07 01:00:49 +0000

    chore(deps): update deno dependencies

--- twind.config.ts
+++ twind.config.ts
@@ -1,6 +1,6 @@
 import { Options } from "$fresh/plugins/twindv1.ts";
 import { defineConfig, Preset } from "https://esm.sh/@twind/core@1.2.0-next-20221226213230"
-import presetTailwind from "https://esm.sh/@twind/preset-tailwind@2.0.0-next-20221213150400"
+import presetTailwind from "https://esm.sh/@twind/preset-tailwind@undefined"
 
 export default {
   ...defineConfig({
